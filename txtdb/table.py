40434725c5cd734465003694c6e2ead1d9e7a000
Alex Vear <av@axvr.io>
2018-11-09 18:55:58 +0000

    Parse each row of the table files

--- txtdb/table.py
+++ txtdb/table.py
@@ -1,3 +1,4 @@
+import re
 from helpers import table_to_file_name
 
 class Table:
@@ -7,6 +8,33 @@ class Table:
     def __build_table(self, table_name, database_dir):
         # TODO error handling
         fh = open(table_to_file_name(database_dir, table_name), "r")
+
+        # Data type
+        col_types = self.__parse_data_types(fh.readline())
+
+        # Column name
+        col_names = self.__parse_column_names(fh.readline())
+
+        # Rows
         for line in fh:
-            print(line) # TODO parse file
+            self.__parse_row(line)
+
         fh.close()
+
+    # TODO handle column attributes
+    def __parse_data_types(self, input_string):
+        return input_string.split(",")
+
+    def __parse_column_names(self, input_string):
+        return map(lambda c: c.replace("\"", ""), input_string.split(","))
+
+    # TODO escape commas in strings
+    # TODO enclose strings in quotes
+    def __parse_row(self, input_string):
+        print(re.split('(?:(?<!\\\),)', input_string.strip()))
+
+    def insert_row(self):
+        return
+
+    def validate_table(self):
+        return
